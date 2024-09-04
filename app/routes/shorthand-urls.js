50a398237399e9147398803f1cf72f95f4a0a561
Allan <Allan.King@hee.nhs.uk>
2024-09-04 11:39:10 +0100

    Add sharthand urls (#7)
    * Add shorturls redirects to express and update references
    
    * Ignore files prefixed with reference
    
    * Fix login page url
    
    * Fix rooting urls

---
+++ app/routes/shorthand-urls.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const shorthandPairs = {
+  '/admin-home': '/prototypes/admin-frameworks',
+  '/login': '/prototypes/login-journey',
+  '/proficiencies': '/prototypes/proficiencies-journey',
+  '/register-delegates': '/prototypes/register',
+};
+
+// Middleware to rewrite shorthand URLs
+router.use((req, res, next) => {
+  let staticUrl = req.path;
+
+  for (const [key, value] of Object.entries(shorthandPairs)) {
+    if (staticUrl.startsWith(key)) {
+      const dynamicUrl = staticUrl.slice(key.length);
+      req.url = `${value}${dynamicUrl}`;  // Rewrite the URL to the full path
+
+      break;
+    }
+  }
+
+  next();
+});
+
+module.exports = router;
