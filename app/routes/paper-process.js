357793501d13fc33fddd0bf02c1a52f5b1becd94
Allan King <Allan.King@hee.nhs.uk>
2024-10-02 12:19:01 +0100

    Update routs to handle multiple entry points for differnt fra mework and tidy up page data and content.

---
+++ app/routes/paper-process.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/choose-framework', (req, res) => {
+  const framework = req.session.data['frameworkName'].replace(/\s+/g, '-').toLowerCase();
+  // str.replace(/\s+/g, '-').toLowerCase();
+
+  res.redirect(`/paper-transfer/_dls-parts/about-${framework}`);
+});
+
+
+router.post('/additional-proficiencies-answer', (req, res) => {
+  const answer = req.session.data['addAdditionalProficiencies'];
+  const framework = req.session.data['frameworkName']
+  let frameworkPostfix = framework ? framework.replace(/\s+/g, '-').toLowerCase() : null;
+  let frameworkPath = '/paper-transfer/additional-proficiencies/add-proficiencies';
+
+  if(framework) {
+    frameworkPath = `/paper-transfer/additional-proficiencies/add-proficiencies-${frameworkPostfix}`
+  }
+
+  if (answer === 'yes') {
+    res.redirect(frameworkPath);
+  } else {
+    res.redirect(`/paper-transfer`);
+  }
+});
+
+module.exports = router;
