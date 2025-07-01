734f2f946bb788e45c403d8a1b4005eaed8af357
Allan <Allan.King@hee.nhs.uk>
2025-07-01 10:39:35 +0100

    Update prototype to version 6.1.0 (#16)
    * Update prototype to use latest version
    
    * Updatte layout to use te-template and fix full width elements
    
    * Add gitignore items back
    
    * Remove mixin working file
    
    * Update service name in config
    
    * Update page layout reference
    
    * add line break to gitignore file
    
    * Update redundant or broken file references and add annotations
    
    * Add container to breadcrumbs
    
    * Update note
    
    * Fix header countainer width

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,4 +1,1 @@
 // ES6 or Vanilla JavaScript
-// import { learningLog } from './learningLog.js';
-
-// const learningLog = new learningLog();
--- app/config.js
+++ app/config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   // Service name
-  serviceName: 'DLS prototype kit',
+  serviceName: 'TEL Prototype',
 
   // Port to run nodemon on locally
-  port: 2000,
+  port: 3000,
 
   // Automatically stores form data, and send to all views
   useAutoStoreData: 'true',
@@ -13,7 +13,4 @@ module.exports = {
   // Enable cookie-based session store (persists on restart)
   // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
   useCookieSessionStore: 'false',
-
-  // Enable or disable built-in docs and examples.
-  useDocumentation: true,
 };
