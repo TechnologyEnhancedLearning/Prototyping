7366d914c219a77caee9d6978da5c6722f0ed0c9
Allan King <Allan.King@hee.nhs.uk>
2026-04-08 13:18:35 +0100

    Add a very basic start page

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'NHSE TEL registration journey',
 
   // Port to run nodemon on locally
   port: 2000,
